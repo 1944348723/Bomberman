47bc8ecad599c00ae63a83396252ec3e55e2cdc8
zcj <1944348723@qq.com>
2026-01-21 16:45:28 +0800

    修复了炸弹从对象池中取出后直接会发生碰撞的bug

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -12,6 +12,7 @@ public class Bomb : MonoBehaviour
     private Tilemap indestructibleWalls;
     private Tilemap destructibleWalls;
     private Rigidbody2D rb;
+    private CircleCollider2D selfCollider;
 
     private float explodeDelayTime = 3f;
     private int explosionLen = 1;
@@ -21,6 +22,7 @@ public class Bomb : MonoBehaviour
     void Awake()
     {
         this.rb = GetComponent<Rigidbody2D>();
+        this.selfCollider = GetComponent<CircleCollider2D>();
         this.explosionsContainer = GameManager.Instance.explosionsContainer;
         this.DestructedWallsContainer = GameManager.Instance.destructedWallsContainer;
         this.indestructibleWalls = GameManager.Instance.indestructibles;
@@ -33,6 +35,7 @@ public class Bomb : MonoBehaviour
         this.explodeDelayTime = delay;
         this.explosionLen = len;
         this.hasExploded = false;
+        selfCollider.isTrigger = true;
         explodeRoutine = StartCoroutine(DelayExplode());
     }
 
@@ -66,8 +69,8 @@ public class Bomb : MonoBehaviour
             // 在FixedUpdate之后，会将Rigidbody2D的位置同步到Transform
             // 所以通过Rigidbody来设置位置只有在FixedUpdate中才有效，否则会被transform覆盖
             // 所以这里直接设置transform.position
-            explosionStart.transform.position = this.rb.position;
-            // Physics2D.SyncTransforms();             // 手动将Transform的位置同步到物理系统
+            explosionStart.transform.position = new (Mathf.RoundToInt(this.rb.position.x), Mathf.RoundToInt(this.rb.position.y));
+            // Physics2D.SyncTransforms();             // 手动将Transform的位置同步到物理系
         }
         SpreadExplosion(cellPos, Vector3Int.up, "ExplosionUpMiddle", "ExplosionUpEnd");
         SpreadExplosion(cellPos, Vector3Int.down, "ExplosionDownMiddle", "ExplosionDownEnd");
@@ -133,5 +136,10 @@ public class Bomb : MonoBehaviour
         Collider2D hit = Physics2D.OverlapPoint(worldPosition, LayerMask.GetMask(this.bombLayerName));
         bomb = hit?.GetComponent<Bomb>();
         return bomb;
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        this.GetComponent<CircleCollider2D>().isTrigger = false;
     }
 }
